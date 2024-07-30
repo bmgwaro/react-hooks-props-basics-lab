af88032363c8ac6e797bbadfaca8d1ba95700519
bmgwaro <brian.gwaro@student.moringaschool.com>
2024-07-30 22:23:46 +0300

    props

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,20 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+function About(props) {
+  const { bio, isPassed, links } = props;
+  if (isPassed) {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        {bio && <p>{bio}</p>}
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links github={links.github} linkedin={links.linkedin} />
+      </div>
+    );
+  }
+
+  return null;
 }
 
 export default About;
